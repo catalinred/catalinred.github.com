4558dff17726a30fae38a1c31f1592fe3d5687e1
Catalin Rosu <red@red-team-design.com>
2014-01-31 23:10:31 +0200

    Gruntfile progress. Fixed sitemap paths.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,33 +2,62 @@ module.exports = function(grunt) {
 
 grunt.initConfig({
 
+  clean: ['_site'],  
+
+  sass: {
+    dist: {
+      options: {
+          style: 'compressed'
+      },
+      files: {
+          'css/style.css': 'saas/style.scss'
+      }
+    } 
+  },
+
   autoprefixer: {
     no_dest: {
       src: 'css/style.css'
     }
+  },  
+
+  // Run Jekyll
+ jekyll: { 
+    serve: {
+      options: {
+        serve: true
+      }
+    }
   },
 
-  sass: {
-      dist: {
-          options: {
-              style: 'compressed'
-          },
-          files: {
-              'css/style.css': 'saas/style.scss'
-          }
-      } 
-  } 
+  watch: {
+      styles: {
+          files: ['sass/*'],
+          tasks: 'sass'
+      },
+      jekyll: {
+        files: ['_site/*'],
+        tasks: ['jekyll:serve']        
+      }
+  }  
 
 });
 
   // Loading Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask( 'default' , [
+      'clean',
       'sass',
-      'autoprefixer'
+      'autoprefixer',
+      'jekyll:serve',
+      'watch:styles',
+      'watch:jekyll'
   ]);
 
 };
