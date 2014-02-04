3a1bc25fa5d2065ad77cd076b1ec3fec5ad67af3
Catalin Rosu <red@red-team-design.com>
2014-02-05 00:01:49 +0200

    - Common.js added - New 300 weight Open sans font added - Initial color scheme variables - Initial styles for master header and footer

---
+++ js/common.js
@@ -0,0 +1,23 @@
+(function(doc, script) {
+    var js, 
+        fjs = doc.getElementsByTagName(script)[0],
+        add = function(url, id) {
+            if (doc.getElementById(id)) {return;}
+            js = doc.createElement(script);
+            js.src = url;
+            id && (js.id = id);
+            fjs.parentNode.insertBefore(js, fjs);
+        };
+
+    // Google Analytics
+    window._gaq = [['_setAccount', 'UA-5924545-2'],['_trackPageview']];
+    add(('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js', 'ga');
+    // Google+ button
+    add('//apis.google.com/js/plusone.js');
+    // Facebook SDK
+    add('//connect.facebook.net/en_US/all.js#xfbml=1&appId=292896510767559', 'facebook-jssdk');
+    // Twitter SDK
+    add('//platform.twitter.com/widgets.js', 'twitter-wjs');
+    // BSA
+    add('//s3.buysellads.com/ac/bsa.js');
+}(document, 'script'));
