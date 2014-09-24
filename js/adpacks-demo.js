ebd9ad84c8fdf8797f21bd609c0d545d9e45e007
Catalin Rosu <redmaniack@gmail.com>
2014-09-24 21:32:20 +0300

    Adpacks for demo pages.

--- js/adpacks-demo.js
+++ js/adpacks-demo.js
@@ -7,22 +7,34 @@ jQuery(function($){
 	var css = '\
 				#adpacks-wrapper{font-family: Arial, Helvetica;width:280px;position:fixed;_position: absolute;bottom: 0;right: 5px;z-index: 9999;background: #eee;padding: 15px;box-shadow: 0 0 2px #444;border-radius: 5px 5px 0 0;}\
 				body .adpacks{background:#fff;padding:15px;margin:15px 0 0;border:3px solid #eee;}\
-				body .one .bsa_it_ad{background:transparent;border:none;font-family:inherit;padding:0;margin:0;}/\
-				body .one .bsa_it_ad .bsa_it_i{display:block;padding:0;float:left;margin:0 10px 0 0;}\
-				body .one .bsa_it_ad .bsa_it_i img{padding:0;border:none;}\
-				body .one .bsa_it_ad .bsa_it_t{padding: 0 0 6px 0; font-size: 11px;}\
-				body .one .bsa_it_p{display:none;}\
-				body #bsap_aplink,body #bsap_aplink:hover{display:block;font-size:9px;margin: -20px 0 0 0;text-align:right;}\
-				body .one .bsa_it_ad .bsa_it_d{font-size: 11px;}\
-				body .one{overflow: hidden}\
+				.adpacks-wrap { \
+				    display: block;\
+				    overflow: hidden;\
+				}\
+				.adpacks-img {\
+				    float: left;\
+				    margin-right: 10px;\
+				    width: 130px;\
+				    height: 100px;\
+				}\
+				.adpacks-text {\
+				    display: block;\
+				    line-height: normal;\
+				    color: #333;\
+				    font-size: 12px;\
+				    text-decoration: none;\
+				    text-align: left;\
+				}\
+				.adpacks-poweredby {\
+				    display: none;\
+				}\
 				';
 
 	addStyle(css);
 
 	// AdPacks markup
 	$('body').append('<div id="adpacks-wrapper">\
-    	<div id="bsap_1257097" class="bsarocks bsap_a5f348233fceef06ba365ab392938d10"></div>\
-	    <a href="http://adpacks.com" id="bsap_aplink">via Ad Packs</a>\
+    	<script async src="//cdn.adpacks.com/adpacks.js?zoneid=1386&serve=C6SI42Y&placement=wwwredteamdesigncom" id="_adpacks_js"></script>\
 	</div>');
 	
 	// AdPacks.com ad code
